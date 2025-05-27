6a05cc3e3da039a6becb558b89db8da2586a3412
Emil Yusupov <mrjava3300@mail.ru>
2025-05-27 14:28:12 +0300

    feat(main: Emiren) - an attempt to implement third and forth part of the document

--- src/main/java/ru/emiren/contest/DTO/Generator/ContestData.java
+++ src/main/java/ru/emiren/contest/DTO/Generator/ContestData.java
@@ -30,4 +30,72 @@ public class ContestData {
     private String researchDirections;
     private String descriptionPlannedScientificResults;
     private String descriptionOfScientificFuture;
+
+    // Third part
+    private String numberOfArticles1;
+    private String numberOfArticles2;
+    private String commentsOnArticles;
+    private String moneyQuantity1;
+    private String moneyQuantity2;
+    private String commentsOnMoney;
+    private String numberOfParticipation1;
+    private String numberOfParticipation2;
+    private String commentsOnPublicationEntering;
+    private String applicationRegistration1;
+    private String applicationRegistration2;
+    private String commentsOnApplicationRegistration;
+    private String RgdRegistration1;
+    private String RgdRegistration2;
+    private String commentsOnRgd;
+    private String numberOfApplications1;
+    private String numberOfApplications2;
+    private String commentsOnNumberOfApplications;
+    private String numberOfSciMem1;
+    private String numberOfSciMem2;
+    private String commentsOnNumberOfSciMem;
+    private String numberOfMembersTill39_1;
+    private String numberOfMembersTill39_2;
+    private String commentsOnNumberOfMembersTill39;
+    private String numberOfStudents1;
+    private String numberOfStudents2;
+    private String commentsOnNumbersOfStudents;
+
+    // Forth Part 1
+    private String fondResult1;
+    private String fondResult2;
+    private String fondResultTotal;
+    private String projectManager1;
+    private String projectManager2;
+    private String projectManagerTotal;
+    private String juniorSciAssoсiate1;
+    private String juniorSciAssoсiate2;
+    private String juniorSciAssoсiateTotal;
+    private String phdStudents1;
+    private String phdStudents2;
+    private String phdStudentsTotal;
+    private String student1;
+    private String student2;
+    private String studentTotal;
+    private String hardware1;
+    private String hardware2;
+    private String hardwareTotal;
+    private String software1;
+    private String software2;
+    private String softwareTotal;
+    private String businessTrip1;
+    private String businessTrip2;
+    private String businessTripTotal;
+    private String outsource1;
+    private String outsource2;
+    private String outsourceTotal;
+    private String otherExpenses1;
+    private String otherExpenses2;
+    private String otherExpensesTotal;
+    private String total1;
+    private String total2;
+    private String total3;
+
+    // Forth Part 2
+    private TableRenderData expendituresTable; // id; position + '/' + status ; quantityOfStaff ; annualFOT
+    private TableRenderData decodingOfExpensesTable; // id ; name ; quantity ; pricePerUnit ; totalSum
 }
--- src/main/java/ru/emiren/contest/Mapper/ContestMapper.java
+++ src/main/java/ru/emiren/contest/Mapper/ContestMapper.java
@@ -46,6 +46,78 @@ public class ContestMapper {
         contestData.setProjectTasks(data.get("projectTasks").toString());
         contestData.setDescriptionPlannedScientificResults(data.get("descriptionPlannedScientificResults").toString());
         contestData.setDescriptionOfScientificFuture(data.get("descriptionOfScientificFuture").toString());
+
+        // Third Part
+
+        contestData.setNumberOfArticles1(data.get("numberOfArticles1") != null ? data.get("numberOfArticles1").toString() : null);
+        contestData.setNumberOfArticles2(data.get("numberOfArticles2") != null ? data.get("numberOfArticles2").toString() : null);
+        contestData.setCommentsOnArticles(data.get("commentsOnArticles") != null ? data.get("commentsOnArticles").toString() : null);
+        contestData.setMoneyQuantity1(data.get("moneyQuantity1") != null ? data.get("moneyQuantity1").toString() : null);
+        contestData.setMoneyQuantity2(data.get("moneyQuantity2") != null ? data.get("moneyQuantity2").toString() : null);
+        contestData.setCommentsOnMoney(data.get("commentsOnMoney") != null ? data.get("commentsOnMoney").toString() : null);
+        contestData.setNumberOfParticipation1(data.get("numberOfParticipation1") != null ? data.get("numberOfParticipation1").toString() : null);
+        contestData.setNumberOfParticipation2(data.get("numberOfParticipation2") != null ? data.get("numberOfParticipation2").toString() : null);
+        contestData.setCommentsOnPublicationEntering(data.get("commentsOnPublicationEntering") != null ? data.get("commentsOnPublicationEntering").toString() : null);
+        contestData.setApplicationRegistration1(data.get("applicationRegistration1") != null ? data.get("applicationRegistration1").toString() : null);
+        contestData.setApplicationRegistration2(data.get("applicationRegistration2") != null ? data.get("applicationRegistration2").toString() : null);
+        contestData.setCommentsOnApplicationRegistration(data.get("commentsOnApplicationRegistration") != null ? data.get("commentsOnApplicationRegistration").toString() : null);
+        contestData.setRgdRegistration1(data.get("RgdRegistration1") != null ? data.get("RgdRegistration1").toString() : null);
+        contestData.setRgdRegistration2(data.get("RgdRegistration2") != null ? data.get("RgdRegistration2").toString() : null);
+        contestData.setCommentsOnRgd(data.get("commentsOnRgd") != null ? data.get("commentsOnRgd").toString() : null);
+        contestData.setNumberOfApplications1(data.get("numberOfApplications1") != null ? data.get("numberOfApplications1").toString() : null);
+        contestData.setNumberOfApplications2(data.get("numberOfApplications2") != null ? data.get("numberOfApplications2").toString() : null);
+        contestData.setCommentsOnNumberOfApplications(data.get("commentsOnNumberOfApplications") != null ? data.get("commentsOnNumberOfApplications").toString() : null);
+        contestData.setNumberOfSciMem1(data.get("numberOfSciMem1") != null ? data.get("numberOfSciMem1").toString() : null);
+        contestData.setNumberOfSciMem2(data.get("numberOfSciMem2") != null ? data.get("numberOfSciMem2").toString() : null);
+        contestData.setCommentsOnNumberOfSciMem(data.get("commentsOnNumberOfSciMem") != null ? data.get("commentsOnNumberOfSciMem").toString() : null);
+        contestData.setNumberOfMembersTill39_1(data.get("numberOfMembersTill39_1") != null ? data.get("numberOfMembersTill39_1").toString() : null);
+        contestData.setNumberOfMembersTill39_2(data.get("numberOfMembersTill39_2") != null ? data.get("numberOfMembersTill39_2").toString() : null);
+        contestData.setCommentsOnNumberOfMembersTill39(data.get("commentsOnNumberOfMembersTill39") != null ? data.get("commentsOnNumberOfMembersTill39").toString() : null);
+        contestData.setNumberOfStudents1(data.get("numberOfStudents1") != null ? data.get("numberOfStudents1").toString() : null);
+        contestData.setNumberOfStudents2(data.get("numberOfStudents2") != null ? data.get("numberOfStudents2").toString() : null);
+        contestData.setCommentsOnNumbersOfStudents(data.get("commentsOnNumbersOfStudents") != null ? data.get("commentsOnNumbersOfStudents").toString() : null);
+
+        // Forth Part 1
+        contestData.setFondResult1(data.get("fondResult1") != null ? data.get("fondResult1").toString() : null);
+        contestData.setFondResult2(data.get("fondResult2") != null ? data.get("fondResult2").toString() : null);
+        contestData.setFondResultTotal(data.get("fondResultTotal") != null ? data.get("fondResultTotal").toString() : null);
+        contestData.setProjectManager1(data.get("projectManager1") != null ? data.get("projectManager1").toString() : null);
+        contestData.setProjectManager2(data.get("projectManager2") != null ? data.get("projectManager2").toString() : null);
+        contestData.setProjectManagerTotal(data.get("projectManagerTotal") != null ? data.get("projectManagerTotal").toString() : null);
+        contestData.setJuniorSciAssoсiate1(data.get("juniorSciAssoсiate1") != null ? data.get("juniorSciAssoсiate1").toString() : null);
+        contestData.setJuniorSciAssoсiate2(data.get("juniorSciAssoсiate2") != null ? data.get("juniorSciAssoсiate2").toString() : null);
+        contestData.setJuniorSciAssoсiateTotal(data.get("juniorSciAssoсiateTotal") != null ? data.get("juniorSciAssoсiateTotal").toString() : null);
+        contestData.setPhdStudents1(data.get("phdStudents1") != null ? data.get("phdStudents1").toString() : null);
+        contestData.setPhdStudents2(data.get("phdStudents2") != null ? data.get("phdStudents2").toString() : null);
+        contestData.setPhdStudentsTotal(data.get("phdStudentsTotal") != null ? data.get("phdStudentsTotal").toString() : null);
+        contestData.setStudent1(data.get("student1") != null ? data.get("student1").toString() : null);
+        contestData.setStudent2(data.get("student2") != null ? data.get("student2").toString() : null);
+        contestData.setStudentTotal(data.get("studentTotal") != null ? data.get("studentTotal").toString() : null);
+        contestData.setHardware1(data.get("hardware1") != null ? data.get("hardware1").toString() : null);
+        contestData.setHardware2(data.get("hardware2") != null ? data.get("hardware2").toString() : null);
+        contestData.setHardwareTotal(data.get("hardwareTotal") != null ? data.get("hardwareTotal").toString() : null);
+        contestData.setSoftware1(data.get("software1") != null ? data.get("software1").toString() : null);
+        contestData.setSoftware2(data.get("software2") != null ? data.get("software2").toString() : null);
+        contestData.setSoftwareTotal(data.get("softwareTotal") != null ? data.get("softwareTotal").toString() : null);
+        contestData.setBusinessTrip1(data.get("businessTrip1") != null ? data.get("businessTrip1").toString() : null);
+        contestData.setBusinessTrip2(data.get("businessTrip2") != null ? data.get("businessTrip2").toString() : null);
+        contestData.setBusinessTripTotal(data.get("businessTripTotal") != null ? data.get("businessTripTotal").toString() : null);
+        contestData.setOutsource1(data.get("outsource1") != null ? data.get("outsource1").toString() : null);
+        contestData.setOutsource2(data.get("outsource2") != null ? data.get("outsource2").toString() : null);
+        contestData.setOutsourceTotal(data.get("outsourceTotal") != null ? data.get("outsourceTotal").toString() : null);
+        contestData.setOtherExpenses1(data.get("otherExpenses1") != null ? data.get("otherExpenses1").toString() : null);
+        contestData.setOtherExpenses2(data.get("otherExpenses2") != null ? data.get("otherExpenses2").toString() : null);
+        contestData.setOtherExpensesTotal(data.get("otherExpensesTotal") != null ? data.get("otherExpensesTotal").toString() : null);
+        contestData.setTotal1(data.get("total1") != null ? data.get("total1").toString() : null);
+        contestData.setTotal2(data.get("total2") != null ? data.get("total2").toString() : null);
+        contestData.setTotal3(data.get("total3") != null ? data.get("total3").toString() : null);
+
+        /*
+            private TableRenderData expendituresTable; // id; position + '/' + status ; quantityOfStaff ; annualFOT
+            private TableRenderData decodingOfExpensesTable; // id ; name ; quantity ; pricePerUnit ; totalSum
+         */
+
+
         return contestData;
     }
 
